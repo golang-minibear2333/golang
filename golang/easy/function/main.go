5387e9b294dfbc3cf4c37c8b47e161e996ab1be7
pzqu <pzqu@qq.com>
2020-04-01 00:38:16 +0800

    [+]函数简单使用

--- golang/easy/function/main.go
+++ golang/easy/function/main.go
@@ -2,4 +2,41 @@
 * @Author: pzqu
 * @Date:   2020-03-31 21:19
 */
-package function
+package main
+
+import "fmt"
+
+//一个函数
+func functionSample() {
+
+}
+
+//一个参数的函数
+func functionParam(num int) {
+}
+
+//多个参数的函数
+func functionParams(a, b int, c string) {
+}
+
+//一个返回值
+func funcReturnOne() int {
+	return 1
+}
+
+//多个返回值
+func funReturnMany() (int, int) {
+	return 1, 2
+}
+
+//返回值有名称
+func funReturnName() (res int) {
+	res = 1 + 1
+	return
+}
+
+func main() {
+	//接收多个返回值
+	a, b := funReturnMany()
+	fmt.Println(a + b)
+}
