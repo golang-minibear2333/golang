a770c49ac3ade19d9a84f5615569a454c88dd5fd
pzqu <pzqu@tencent.com>
2020-05-27 23:23:20 +0800

    简单定义错误

--- golang/easy/errors/error1.go
+++ golang/easy/errors/error1.go
@@ -11,7 +11,7 @@ import (
 	"fmt"
 )
 
-func errorDemo() (int,error){
+func errorDemo() (int, error) {
 	err := errors.New("这是一个自定义错误")
 
 	printErrMessage(err)
@@ -29,10 +29,16 @@ func errorDemo() (int,error){
 
 	printErrMessage(err)
 
-	return 0,err
-
+	return 0, err
 }
 
+func divisionInt(a, b int) (int, error) {
+	if b == 0 {
+		return -1, errors.New("除数不能为0")
+	}
+
+	return a / b, nil
+}
 func printErrMessage(err error) {
 	// 判断上一个语句返回，是否包含错误的过程有个学名叫：卫述语句
 	// go 语言会有一大堆这种语句，在go2中会被 "check..handle" 语句修复
@@ -41,12 +47,24 @@ func printErrMessage(err error) {
 	}
 }
 
-func main() {
+
+func testDemo(){
 	//根据Golang的约定，每个可能导致错误的函数都将error其作为最后一个返回值，码农有责任在每一步都正确处理它
-	_,err := errorDemo()
-	if err!=nil{
+	_, err := errorDemo()
+	if err != nil {
 		fmt.Println(err)
 		return
 	}
 	fmt.Println("没有错误返回，继续执行")
+}
+func main() {
+
+	testDemo()
+	a, b := 4, 0
+	res, err := divisionInt(a, b)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	fmt.Println(a, "除以", b, "的结果是 ", res)
 }
---
+++ golang/easy/errors/error2.go
@@ -0,0 +1,6 @@
+/*
+* @Title:  
+* @Author: pzqu
+* @Date:   2020-05-27 23:22
+*/
+package main
